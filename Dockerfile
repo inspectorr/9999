3a2f13a02e777e08fdf4398335437f445f1f0308
Roman Verkhoturov <hmrfins@gmail.com>
2023-11-19 18:31:05 +0300

    initial django app

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.10-slim as python-builder
+
+WORKDIR /app
+
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+COPY requirements.txt .
+
+RUN pip install --no-cache -r requirements.txt
+
+COPY . .
+
+RUN chmod +x entrypoint.sh
+
+ENTRYPOINT ["sh", "entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+action=$1
+if [ "$action" = "runserver" ]; then
+  python manage.py collectstatic --no-input &&
+  python manage.py migrate &&
+  gunicorn \
+    --bind 0.0.0.0:8000 \
+    --workers 3 \
+    --error-logfile - \
+    --access-logfile - \
+    --timeout 60 \
+    --reload \
+    application.wsgi:application
+fi
